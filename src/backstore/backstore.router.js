cc27d6a5222cfa6883c9ddcec12e231caff9fc10
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-08 01:08:02 -0300

    Validação de `ObjectId` através de Middleware

--- src/backstore/backstore.router.js
+++ src/backstore/backstore.router.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const controller = require('./backstore.controller');
+const { validateObjectId } = require('../db/database.helper');
 
 const router = express.Router()
 
 router.get('/', controller.readAll)
-router.get('/:id', controller.readById)
+router.get('/:id', validateObjectId, controller.readById)
 router.post('/', controller.create)
-router.put('/:id', controller.updateById)
-router.delete('/:id', controller.deleteById)
-router.patch('/:id/sale', controller.saleById)
+router.put('/:id', validateObjectId, controller.updateById)
+router.delete('/:id', validateObjectId, controller.deleteById)
+router.patch('/:id/sale', validateObjectId, controller.saleById)
 
 module.exports = router
