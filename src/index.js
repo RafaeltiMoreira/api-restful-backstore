5483073a4e74e2f170e1a79bf5e656de13b07930
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-08 00:51:21 -0300

    Implementação de erros inesperados

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { connectToDatabase } = require('./db/database-connection');
 const cors = require('cors');
+
 const backstoreRouter = require('./backstore/backstore.router');
 
 async function main() {
@@ -16,6 +17,15 @@ async function main() {
   })
 
   app.use('/backstore', backstoreRouter)
+
+  app.use(function (err, _, res, _) {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Algo deu errado!' })
+  })
+
+  app.use('*', (_, res) => {
+    res.status(404).send({ error: 'Endpoint não encontrado.' })
+  })
 
   app.listen(3000, function () {
     console.log('Servidor rodando em http://localhost:3000')
